3f05272157d8712a754d7e910c87a04a505fa086
H4rakiri <popovartem2907@gmail.com>
2023-11-08 01:06:07 +0300

    lab1 almost ready

--- OpenGLrememberProj/Render.cpp
+++ OpenGLrememberProj/Render.cpp
@@ -1,5 +1,3 @@
-
-
 #include "Render.h"
 
 #include <Windows.h>
@@ -8,7 +6,35 @@
 
 void Render(double delta_time)
 {    
+	glBegin(GL_TRIANGLES);
+	glColor3d(0, 1, 0);
+	glVertex3d(0, 0, 0);
+	glVertex3d(2, -4, 0);
+	glVertex3d(3, -4, -3);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3d(0, 0, 0);
+	glVertex3d(2, -4, 0);
+	glVertex3d(8, -1, 0);
+	
+	glVertex3d(8, -1, 0);
+	glVertex3d(11, -4, 0);
 
+	glVertex3d(11, -4, 0);
+	glVertex3d(9, 0, 0);
+	
+	glVertex3d(9, 0, 0);
+	glVertex3d(14, 4, 0);
 
+	glVertex3d(14, 4, 0);
+	glVertex3d(8, 2, 0);
+
+	glVertex3d(8, 2, 0);
+	glVertex3d(4, 6, 0);
+
+	glColor3d(0.5, 0.02, 0.9);
+	glVertex3d(4, 6, 0);
+	glVertex3d(0, 0, 0);
+	glEnd();
 }   
 
