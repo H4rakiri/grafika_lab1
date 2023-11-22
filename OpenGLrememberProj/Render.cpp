0c0a0595a1ab32591ef8e883e22e5341ba9cc89a
H4rakiri <popovartem2907@gmail.com>
2023-11-22 21:48:24 +0300

    kg lab1

--- OpenGLrememberProj/Render.cpp
+++ OpenGLrememberProj/Render.cpp
@@ -6,35 +6,129 @@
 
 void Render(double delta_time)
 {    
-	glBegin(GL_TRIANGLES);
-	glColor3d(0, 1, 0);
-	glVertex3d(0, 0, 0);
-	glVertex3d(2, -4, 0);
-	glVertex3d(3, -4, -3);
+	double V0[] = {0, 0, 0};
+	double V1[] = {2, -4, 0};
+	double V2[] = {8, -1, 0};
+	double V3[] = {11, -4, 0}; 
+	double V4[] = {9, 0, 0};
+	double V5[] = {14, 4, 0};
+	double V6[] = {8, 2, 0};
+	double V7[] = {4, 6, 0};
+
+	double V00[] = { 0, 0, 3 };
+	double V11[] = { 2, -4, 3 };
+	double V22[] = { 8, -1, 3 };
+	double V33[] = { 11, -4, 3 };
+	double V44[] = { 9, 0, 3 };
+	double V55[] = { 14, 4, 3 };
+	double V66[] = { 8, 2, 3 };
+	double V77[] = { 4, 6, 3 };
+
+	glBegin(GL_POLYGON);
+	glColor3d(1, 0, 0);
+	glVertex3dv(V0); //v0
+
+	glVertex3dv(V1); //v1
+
+	glVertex3dv(V2); //v2
+	
+	glVertex3dv(V6); //v6
+
+	glVertex3dv(V7); //v7
 	glEnd();
 	glBegin(GL_TRIANGLES);
-	glColor3d(0, 0, 0);
-	glVertex3d(2, -4, 0);
-	glVertex3d(8, -1, 0);
+	glVertex3dv(V2); 
+
+	glVertex3dv(V3); 
 	
-	glVertex3d(8, -1, 0);
-	glVertex3d(11, -4, 0);
+	glVertex3dv(V6); 
+	glEnd();
 
-	glVertex3d(11, -4, 0);
-	glVertex3d(9, 0, 0);
+	glBegin(GL_TRIANGLES);
+	glVertex3dv(V4);
+
+	glVertex3dv(V5);
+
+	glVertex3dv(V6);
+	glEnd();
 	
-	glVertex3d(9, 0, 0);
-	glVertex3d(14, 4, 0);
+	
+	glBegin(GL_QUADS);
+	glColor3d(0, 1, 1);
+	glVertex3dv(V0);
+	glVertex3dv(V00);
+	glVertex3dv(V11);
+	glVertex3dv(V1);
+	glEnd();
+	
+	glBegin(GL_QUADS);
+	glVertex3dv(V1);
+	glVertex3dv(V11);
+	glVertex3dv(V22);
+	glVertex3dv(V2);
+	glEnd();
+	
+	glBegin(GL_QUADS);
+	glVertex3dv(V2);
+	glVertex3dv(V22);
+	glVertex3dv(V33);
+	glVertex3dv(V3);
+	glEnd();
 
-	glVertex3d(14, 4, 0);
-	glVertex3d(8, 2, 0);
+	glBegin(GL_QUADS);
+	glVertex3dv(V3);
+	glVertex3dv(V33);
+	glVertex3dv(V44);
+	glVertex3dv(V4);
+	glEnd();
 
-	glVertex3d(8, 2, 0);
-	glVertex3d(4, 6, 0);
+	glBegin(GL_QUADS);
+	glVertex3dv(V4);
+	glVertex3dv(V44);
+	glVertex3dv(V55);
+	glVertex3dv(V5);
+	glEnd();
 
-	glColor3d(0.5, 0.02, 0.9);
-	glVertex3d(4, 6, 0);
-	glVertex3d(0, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3dv(V5);
+	glVertex3dv(V55);
+	glVertex3dv(V66);
+	glVertex3dv(V6);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex3dv(V6);
+	glVertex3dv(V66);
+	glVertex3dv(V77);
+	glVertex3dv(V7);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex3dv(V7);
+	glVertex3dv(V77);
+	glVertex3dv(V00);
+	glVertex3dv(V0);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3d(0, 1, 0);
+	glVertex3dv(V00);
+	glVertex3dv(V11);
+	glVertex3dv(V22);
+	glVertex3dv(V66);
+	glVertex3dv(V77);
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	glVertex3dv(V22);
+	glVertex3dv(V33);
+	glVertex3dv(V66);
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	glVertex3dv(V44);
+	glVertex3dv(V55);
+	glVertex3dv(V66);
 	glEnd();
 }   
 
